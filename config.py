51ba470e1b5ae76dac94c79030dc00ee49a7ca8b
SKY <sky012877@gmail.com>
2023-02-11 22:02:41 -0800

    config.py - Added :

---
+++ config.py
@@ -0,0 +1,22 @@
+STRUCTURE = {
+    "URL": "",
+    "OBJECT": {
+        "_object_name": [
+            {
+                'by': '',
+                'attr': '',
+                'many': '',
+                'position': '',
+            }]
+    },
+    "ACTIONS": {
+        "_actions_name": [
+            {
+                'obj': '',
+                'cmd': '',
+                'data': '',
+            }
+        ]
+    },
+
+}
