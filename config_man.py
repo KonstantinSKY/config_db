8f7c2a207e6ad1631a4272c4f830c9ed5b850ade
SKY <sky012877@gmail.com>
2023-02-16 16:36:27 -0800

    config_man.py - Modified :

--- config_man.py
+++ config_man.py
@@ -1,38 +1,26 @@
 from berkeleydb import db
-import json
+import json, os
+from helpers import yes_or_no
+import importlib
+from bdb import BDB
 
 
-class ConfigMan:
+class ConfigMan(BDB):
 
-    def __init__(self, db_name):
-        self.DB = db.DB()
-        self.DB.open(db_name, None, db.DB_HASH, db.DB_CREATE)
-        self.curDB = self.DB.cursor()
+    def __init__(self, file_name):
+        super().__init__(file_name)
+        self.root = None
+        self.configs = None
+        self.sub_config = None
+        self.cursor = None
+        self.reinit()
+
+    def reinit(self):
         self.root = "Configs"
-        self.configs = {self.root: self.read_db()}
+        self.configs = {self.root: self.read_all_db()}
         self.sub_config = self.configs[self.root]
         self.cursor = list(self.configs.keys())
         print("self.cursor", self.cursor)
-
-    @staticmethod
-    def bin_to(_bin):
-        obj = _bin.decode()
-        try:
-            obj = json.loads(obj)
-        except ValueError:
-            print(obj, "is not json")
-        return obj
-
-    def read_db(self):
-        record = self.curDB.first()
-        configs = {}
-        while record:
-            print("Record:", record)
-            key = self.bin_to(record[0])
-            val = self.bin_to(record[1])
-            configs[key] = val
-            record = self.curDB.next()
-        return configs
 
     def get_sub_config(self):
         self.sub_config = self.configs[self.root]
@@ -46,6 +34,48 @@ class ConfigMan:
 
     def create(self):
         print("Create function")
+        if self.cursor[-1] == self.root:
+            self.create_config()
+            self.reinit()
+
+    def create_config(self):
+        print("Creating config ...")
+        name = input("Input config name : ")
+        print("You can load config template from config file")
+        print("config json structure inside the file must have the same name like config:", name)
+        file_name = input("Input config structure template .py file: ")
+        config = {name: {}}
+        if not file_name or not os.path.isfile(file_name):
+            print("Wrong file name:", file_name)
+            if yes_or_no("Do your want to create config without template with empty {} and to add it to DB?"):
+                self.add_db(config)
+            return
+
+        module = importlib.import_module(file_name.rstrip(".py"))
+        if not hasattr(module, name):
+            print("The config file does not contain root structure: ", name)
+            print(module.__dir__())
+            if yes_or_no("Do your want to create config without template with empty {} and to add it to DB?"):
+                self.add_db(config)
+            return
+
+        print(config)
+        print(getattr(module, name))
+        # config[name] = {"template": module.name}
+        print("New template for :", name)
+        obj = getattr(module, name)
+        print(json.dumps(obj, indent=4)) if isinstance(obj, dict) or isinstance(obj, list) else obj
+        if not yes_or_no("Do your want to create config with template and add it to DB?"):
+            return
+        config[name]["_template"] = getattr(module, name)
+        print(config[name])
+        self.add_db(config)
+
+    def delete(self, key):
+        pass
+    def show(self):
+        print("Element", self.cursor[-1], "structure:")
+        print(self.cursor[-1], " = ", json.dumps(self.sub_config, indent=2))
 
     def back(self):
         self.cursor.pop()
@@ -56,20 +86,24 @@ class ConfigMan:
         self.DB.close()
         exit()
 
+    
     def menu(self):
 
         print("=" * 100)
         print("MENU : ", self.cursor[-1:][0])
         print("=" * 100)
-        act_keys = {"C": {"cmd": "create", "descr": "Create new"},
-                    "B": {"cmd": "back", "descr": "Back to "},
+        act_keys = {"C": {"cmd": self.create, "descr": "Create new"},
+                    "F": {"cmd": self.fill, "descr": "Fill one"},
+                    "D": {"cmd": self.delete, "descr": "Delete one"},
+                    "S": {"cmd": self.show, "descr": "Show element"},
+                    "B": {"cmd": "back", "descr": f"Back to "},
                     "E": {"cmd": "exit", "descr": "Exit program"}
                     }
         # Print act menu
         act_menu = []
         for act_key in act_keys:
             letter = list(act_key)[0]
-            if letter == "B" and len(self.cursor) <= 1:
+            if (letter == "B" or letter == "D") and len(self.cursor) <= 1:
                 continue
             act_menu.append(letter)
             print(f"[ {letter} ] - {act_keys[act_key]['descr']}")
@@ -100,8 +134,10 @@ class ConfigMan:
             print("Incorrect select! Please try one more time.")
             return self.menu()
 
-        cmd = "self. " + act_keys[select]["cmd"] + "()"
-        eval(cmd)
+        act_keys[select]["cmd"]()
+        #cmd = "self. " + act_keys[select]["cmd"] + "()"
+        #eval(cmd)
+
         return self.menu()
 
     def __delete__(self, instance):
