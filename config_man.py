0b5c20eeff890d3f4c0adadfbf09f3607f797e63
SKY <sky012877@gmail.com>
2023-02-21 23:56:19 -0800

    config_man.py - Modified :

--- config_man.py
+++ config_man.py
@@ -8,141 +8,110 @@ from dyn_menu import DynMenu
 
 class ConfigMan(BDB):
 
-    def __init__(self, file_name):
+    def __init__(s, file_name):
         super().__init__(file_name)
-        self.root = None
-        self.configs = None
-        self.sub_config = None
-        self.cursor = None
-        self.reinit()
-        self.main_menu = DynMenu("MENU")
+        s.root = "Config"
+        s.configs = {s.root: s.read_all_db()}
+        s.sub_config = s.configs[s.root]
+        s.cursor = list(s.configs.keys())
+        # s.reinit()
+        s.main_menu = DynMenu("MENU")
 
-    def reinit(self):
-        self.root = "Configs"
-        self.configs = {self.root: self.read_all_db()}
-        self.sub_config = self.configs[self.root]
-        self.cursor = list(self.configs.keys())
-        print("self.cursor", self.cursor)
+    # def reinit(s):
+    #     s.configs = {s.root: s.read_all_db()}
+    #     s.sub_config = s.configs[s.root]
+    #     s.cursor = list(s.configs.keys())
+    #     print("self.cursor", s.cursor)
 
-    def get_sub_config(self):
-        self.sub_config = self.configs[self.root]
-        print(self.cursor)
-        if len(self.cursor) <= 1:
+    def get_sub_config(s):
+        s.sub_config = s.configs[s.root]
+        if len(s.cursor) <= 1:
             return
-        for key in self.cursor[1:]:
+        for key in s.cursor[1:]:
             print("Check key:", key)
-            self.sub_config = self.sub_config[key]
-        print(self.sub_config)
+            s.sub_config = s.sub_config[key]
 
-    def sub_menu(self, variants):
-        for key, val in variants.items():
-            print(f"[{key}] - {val['descr']}")
-
-        select = input("Enter your select: ")
-        if select not in variants:
-            print("Incorrect select! Please try one more time.")
-            self.sub_menu(variants)
-        else:
-            return variants[select]
-
-        print("Select structure type:")
-
-    # def create(self):
-    #     print("Create function")
-    #     if self.cursor[-1] == self.root:
-    #         self.create_config()
-    #         self.reinit()
     def enter_name(self):
         return input(f"Input NEW {self.cursor[-1:][0].rstrip('s')} name : ")
 
-    def enter_name_clear(self):
-        self.add_db({self.enter_name(): self.select_type()})
+    def save_active_config(s):
+        s.add_db({s.cursor[1]: s.configs[s.root][s.cursor[1]]})
 
-    def enter_name_templ(self):
-        self.add_db({self.enter_name(): self.add_template()})
+    def enter_name_clear(s):
+        curs, root, sub_conf, confs = s.cursor, s.root, s.sub_config, s.configs
 
-    def enter_name_templ(self):
-        pass
+        name = s.enter_name()
+        if curs[-1] == root:
+            print(curs[-1], "must be only {} json or dictionary format, the object will be create automatically!")
+            sub_conf[name] = {}
+            s.add_db({name: {}})
+
+        else:
+            sub_conf[name] = s.select_type()
+            s.save_active_config()
+        print(sub_conf[name])
+        if not (isinstance(sub_conf[name], dict) or isinstance(sub_conf[name], list)):
+            return
+        curs.append(name)
+        s.get_sub_config()
 
     def update_name(self):
         pass
 
     def create(self):
         parent = self.cursor[-1:][0]
+
         print("Creating NEW", parent)
         # name = input(f"Input NEW {parent} name : ")
 
         create_menu_keys = {
-            "NC": {"cmd": self.enter_name_clear, "descr": "Enter New Name (or Key) + Clear object"},
-            "NT": {"cmd": self.enter_name_templ, "descr": "Enter New Name (or Key) + Template object"},
+            "N": {"cmd": self.enter_name_clear, "descr": f"Enter New {parent} : Name (or Key) + clear object"},
+            "AT": {"cmd": self.add_templ, "descr": f"Add new template for config: {parent}"},
             "U": {"cmd": self.update_name, "descr": "Update template name and add template part to fill"},
-            "T": {"cmd": self.add_template, "descr": "Create from Template"},
+            "T": {"cmd": self.add_templ, "descr": "Create from Template"},
             "C": {"cmd": self.select_type, "descr": "Create from Clear structure"},
             "B": {"cmd": self.back, "descr": "Back to previous menu"},
         }
 
-        create_menu = DynMenu("MENU - Creating new config")
+        create_menu = DynMenu(f"MENU - Creating new {parent}")
+        create_menu_keys["AT"]["hide"] = True if len(self.cursor) != 2 else False
         create_menu.add_obj(create_menu_keys)
         create_menu.activate()["cmd"]()
         return
-        print("You can load config template from config file")
-        if yes_or_no(f"Do your want to create {name} element from config file?"):
-            self.add_template(self, name)
-        else:
-            self.add_db({name: self.select_type()})
-
-        # self.add_db(config)
-        self.cursor = [self.root, name]
-        return
-        # self.empty()
-        # self.add_db(self.config)
 
     def get_file(self):
         pass
 
-    # import os
-    #
-    # for x in os.listdir():
-    #     if x.endswith(".txt"):
-    #         # Prints only text file present in My Folder
-    #         print(x)
+    def add_templ(s):
+        curs, sub_conf = s.cursor, s.sub_config
 
-
-
-    def add_template(self, name):
-        print("config json structure inside the file must have the same name like config:", name)
-        file_name = input("Input config structure template .py file: ")
+        file_name = input("Input config structure template .json file:")
         if not file_name or not os.path.isfile(file_name):
             print("Wrong file name:", file_name)
-            return self.create_config()
-
-        self.get_file()
-        # read json config template
-
-        module = importlib.import_module(file_name.rstrip(".py"))
-        if not hasattr(module, name):
-            print("The config file does not contain root structure: ", name)
-            print(module.__dir__())
-            return self.create_config()
-
-        print("New template for :", name)
-        obj = getattr(module, name)
-        print(json.dumps(obj, indent=4)) if isinstance(obj, dict) or isinstance(obj, list) else obj
-        if not yes_or_no("Do your want to create config with template and add this structure  to DB?"):
             return
+        with open(file_name, "r") as f:
+            template = json.load(f)
 
-        config[name]["_template"] = getattr(module, name)
-        print(config[name])
-        self.add_db(config)
+        print("Template :")
+        print(json.dumps(template, indent=4))
+        if not yes_or_no(f"Do you want to add to config : {curs}"):
+            return
+        sub_conf.update(template)
+        s.save_active_config()
+        s.get_sub_config()
+        return
 
-    def select_type(self):
+    @staticmethod
+    def select_type():
         type_menu_keys = {
             "D": {"cmd": {}, "descr": "{dict} - Dictionary or hash"},
             "L": {"cmd": [], "descr": "[list -  List or array"},
             "S": {"cmd": "", "descr": "{dict} - String"},
             "I": {"cmd": int(), "descr": "int() = 0 - Integer number"},
             "F": {"cmd": float(), "descr": "float() = 0.0 - Float number"},
+            "N": {"cmd": None, "descr": "None"},
         }
+
         type_menu = DynMenu("MENU - type of config element  ")
 
         type_menu.add_obj(type_menu_keys)
@@ -167,41 +136,63 @@ class ConfigMan(BDB):
         self.DB.close()
         exit()
 
-    def menu(self):
-        self.main_menu.name = self.cursor[-1:][0]
-        act_keys = {"C": {"cmd": self.create, "descr": "Create new"},
-                    "F": {"cmd": self.fill, "descr": "Fill one"},
-                    "D": {"cmd": self.delete, "descr": "Delete one"},
-                    "S": {"cmd": self.show, "descr": "Show element"},
-                    "B": {"cmd": self.back, "descr": f"Back to "},
-                    "E": {"cmd": self.exit, "descr": "Exit program"}
+    def menu(s):
+        curs, sub_conf = s.cursor, s.sub_config
+        s.main_menu.name = f"{curs[-1]} {' ' * 20} {sub_conf}"
+        act_keys = {"C": {"cmd": s.create, "descr": f"Create new object in {curs[-1]} "},
+                    "T": {"cmd": s.add_templ, "descr": f"Add new template for config: {curs[-1]}", "hide": True},
+                    "F": {"cmd": s.fill, "descr": "Fill one"},
+                    "D": {"cmd": s.delete, "descr": "Delete one"},
+                    "S": {"cmd": s.show, "descr": "Show object"},
+                    "B": {"cmd": s.back, "descr": f"Back to "},
+                    "E": {"cmd": s.exit, "descr": "Exit program"}
                     }
         for key, val in act_keys.items():
-            if (key == "B" or key == "D") and len(self.cursor) <= 1:
-                act_keys[key]["hide"] = True
-                continue
-            if key == "F" and "_template" not in self.sub_config:
-                act_keys[key]['hide'] = True
+            if (key == "B" or key == "D") and len(curs) <= 1:
+                val["hide"] = True
                 continue
 
-        self.main_menu.add_obj(act_keys)
+            if key == "T" and len(curs) == 2 and "_template" not in sub_conf:
+                val["hide"] = False
+                continue
+
+            if key == "F" and "_template" not in sub_conf:
+                val['hide'] = True
+                continue
+
+        s.main_menu.add_obj(act_keys)
         # Additional dynamic menu
         addition_menu = {}
 
-        for idx, key in enumerate(self.sub_config.keys()):
-            if isinstance(self.sub_config[key], list) or isinstance(self.sub_config[key], dict):
-                addition_menu[str(idx)] = {"descr": str(key)}
+        if isinstance(sub_conf, dict):
+            num = 1
+            for key, config in sub_conf.items():
+                addition_menu[str(num)] = {"descr": key}
+                num += 1
 
-        self.main_menu.add_obj(addition_menu)
-        select = self.main_menu.activate()
+        if isinstance(sub_conf, list):
+            num = 1
+            for idx, config in enumerate(sub_conf):
+                addition_menu[idx] = {"descr": config}
+                num += 1
+
+        s.main_menu.add_obj(addition_menu)
+        select = s.main_menu.activate()
+
         if "cmd" in select and callable(select["cmd"]):
             select["cmd"]()
         else:
-            self.cursor.append(select['descr'])
+            print("select", select)
+            if isinstance(sub_conf, dict):
+                s.cursor.append(select['descr'])
+            if isinstance(sub_conf, list):
+                s.cursor.append(int(select['key']))
 
-        self.main_menu.clear()
-        self.get_sub_config()
-        return self.menu()
+            print("cursor::;", s.cursor)
+
+        s.main_menu.clear()
+        s.get_sub_config()
+        return s.menu()
 
     def __delete__(self, instance):
         self.DB.close()
